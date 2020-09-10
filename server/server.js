1f323a04493840dd968e4402500ed224c4d57d71
david35008 <67857450+david35008@users.noreply.github.com>
2020-09-10 16:25:37 +0300

    add DELETE album by id entry point
    a DELETE request to /album/123 delete the album 123

--- server/server.js
+++ server/server.js
@@ -314,6 +314,13 @@ app.delete("/artist/:id", (req, res) => {
   });
 })
 
+app.delete("/album/:id", (req, res) => {
+  connection.query(`DELETE FROM albums WHERE album_ID= ${req.params.id}`, (err, result) => {
+      if (err) res.send("An error occurred.");
+      res.send("One album deleted");
+  });
+})
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
