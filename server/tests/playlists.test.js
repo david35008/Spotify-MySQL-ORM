bda293763cfd7baad7deaf70615005177b3ea55d
david35008 <67857450+david35008@users.noreply.github.com>
2020-09-29 01:58:32 +0300

    Update playlists.test.js

--- server/tests/playlists.test.js
+++ server/tests/playlists.test.js
@@ -1,31 +1,163 @@
 
+/**
+ * @jest-environment node
+ */
+
+require('dotenv').config();
 const request = require('supertest');
 const server = require('../server');
 const { Playlist } = require('../models');
+const { Op } = require("sequelize");
 
 const playlistMock = {
-  name: 'new playlist name'
+  name: 'new playlist name',
 }
 
+const playlistChaengeMock = {
+  name: 'change playlist name'
+}
+
+const userMock = {
+  email: process.env.LOGIN,
+  password: process.env.PASSWORD,
+  rememberToken: false
+}
+
+let header;
+
 describe('check playlists routs', () => {
-    afterAll(async () => {
-        await server.close();
-        done();
-    });
-  beforeEach(async () => {
+  beforeAll(async () => {
+    const response = await request(server)
+      .post("/users/logIn")
+      .send(userMock)
+      .expect(200);
+    header = response.header;
+  })
+  afterAll(async () => {
+    await server.close();
+  });
+  afterEach(async () => {
     await Playlist.destroy({ truncate: true, force: true });
   });
 
-  it('Can create playlist', async () => {
-    const { body } = await request(server).post('/api/v1/playlists').send(playlistMock);
-    expect(body.name).toBe(playlistMock.name)
+  it('Can get all playlists', async () => {
+    const { body: newPlaylist } = await request(server)
+      .post('/api/v1/playlists')
+      .set('Authorization', header['authorization'])
+      .send(playlistMock)
+      .expect(200);
+
+    const { body: getAllPlaylists } = await request(server)
+      .get(`/api/v1/playlists`)
+      .set('Authorization', header['authorization'])
+      .expect(200);
+
+    expect(getAllPlaylists.length > 0).toBe(true)
+    const playlistsFromDB = await Playlist.findAll();
+    expect(playlistsFromDB.length > 0).toBe(true)
+
+    expect(playlistsFromDB.length).toBe(getAllPlaylists.length)
   })
 
-  it('Can get single playlist', async () => {
-    const { body: newPlaylist } = await request(server).post('/api/v1/playlists').send(playlistMock);
-    const { body: getSinglePlaylistResponseBody } = await request(server).get(`/api/v1/playlists/byId/${newPlaylist.id}`);
+  it('Can get single playlist by id', async () => {
+    const { body: newPlaylist } = await request(server)
+      .post('/api/v1/playlists')
+      .set('Authorization', header['authorization'])
+      .send(playlistMock)
+      .expect(200);
 
-    expect(getSinglePlaylistResponseBody.name).toBe(playlistMock.name)
-    expect(getSinglePlaylistResponseBody.id).toBe(newPlaylist.id)
+    const { body: getSinglePlaylist } = await request(server)
+      .get(`/api/v1/playlists/byId/${newPlaylist.id}`)
+      .set('Authorization', header['authorization'])
+      .expect(200);
+
+    const playlistFromDB = await Playlist.findByPk(newPlaylist.id);
+    expect(playlistFromDB.name).toBe(playlistMock.name)
+    expect(getSinglePlaylist.name).toBe(playlistMock.name);
+    expect(getSinglePlaylist.id).toBe(newPlaylist.id);
+  })
+
+  it('Can get single playlist by name', async () => {
+    const { body: newPlaylist } = await request(server)
+      .post('/api/v1/playlists')
+      .set('Authorization', header['authorization'])
+      .send(playlistMock)
+      .expect(200);
+
+    const { body: getSinglePlaylist } = await request(server)
+      .get(`/api/v1/playlists/byName/${newPlaylist.name}`)
+      .set('Authorization', header['authorization'])
+      .expect(200);
+
+    const playlistFromDB = await Playlist.findAll({ where: { name: { [Op.like]: `%${playlistMock.name}%` } } });
+    expect(playlistFromDB[0].name).toBe(newPlaylist.name)
+    expect(getSinglePlaylist[0].name).toBe(playlistMock.name);
+    expect(getSinglePlaylist[0].id).toBe(newPlaylist.id);
+  })
+
+  it('Can get top playlists', async () => {
+    for (let i = 0; i < 21; i++) {
+      playlistMock.name = `new playlist name ${i}`
+      await request(server)
+        .post('/api/v1/playlists')
+        .set('Authorization', header['authorization'])
+        .send(playlistMock)
+        .expect(200);
+    }
+
+    const { body: getTopPlaylists } = await request(server)
+      .get(`/api/v1/playlists/top`)
+      .set('Authorization', header['authorization'])
+      .expect(200);
+
+    expect(getTopPlaylists.length <= 20).toBe(true)
+    const topPlaylistsFromDB = await Playlist.findAll({ limit: 20 });
+    expect(topPlaylistsFromDB.length <= 20).toBe(true);
+
+    expect(topPlaylistsFromDB.length).toBe(getTopPlaylists.length)
+  })
+
+  it("Can create playlist", async () => {
+    const { body: newPlaylist } = await request(server)
+      .post('/api/v1/playlists')
+      .set('Authorization', header['authorization'])
+      .send(playlistMock)
+      .expect(200);
+
+    const playlistFromDB = await Playlist.findByPk(newPlaylist.id);
+    expect(playlistFromDB.name).toBe(newPlaylist.name)
+  });
+
+  it('Can change playlist', async () => {
+    const { body: newPlaylist } = await request(server)
+      .post('/api/v1/playlists')
+      .set('Authorization', header['authorization'])
+      .send(playlistMock)
+      .expect(200);
+
+    await request(server)
+      .put(`/api/v1/playlists/${newPlaylist.id}`)
+      .set('Authorization', header['authorization'])
+      .send(playlistChaengeMock)
+      .expect(200);
+
+    const playlistFromDB = await Playlist.findByPk(newPlaylist.id);
+    expect(playlistFromDB.name).toBe(playlistChaengeMock.name)
+  })
+
+  it('Can delete playlist', async () => {
+    const { body: newPlaylist } = await request(server)
+      .post('/api/v1/playlists')
+      .set('Authorization', header['authorization'])
+      .send(playlistMock)
+      .expect(200);
+
+    await request(server)
+      .delete(`/api/v1/playlists/${newPlaylist.id}`)
+      .set('Authorization', header['authorization'])
+      .expect(200);
+
+    const playlistFromDB = await Playlist.findByPk(newPlaylist.id);
+    expect(playlistFromDB).toBe(null)
   })
 })
